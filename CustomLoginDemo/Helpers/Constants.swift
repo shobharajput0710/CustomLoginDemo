d96ab620cbb0059fb0e44b659f98863659bc8729
shobharajput0710 <shobharajput0710@gmail.com>
2019-10-03 18:01:40 +0530

    Login Demo

---
+++ CustomLoginDemo/Helpers/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  CustomLoginDemo
+//
+// Created by shobha kumari on 2019-09-29.
+//  Copyright © 2019 shobha kumari. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Storyboard {
+        
+        static let homeViewController = "HomeVC"
+        
+    }
+    
+    
+}
---
+++ CustomLoginDemo/View Controllers/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  CustomLoginDemo
+//
+//  Created by shobha kumari on 2019-09-29.
+//  Copyright © 2019 shobha kumari. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
